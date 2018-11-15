278dbaab554b41b039f84502ec2ffe7ca2d910b0
JeevDeveloper <jeeevdeveloper@gmail.com>
2018-11-15 15:36:14 +0100

    Added methods for finding all and finding Auto by color.

--- src/main/java/de/hhu/propra/db/data/KundeRepository.java
+++ src/main/java/de/hhu/propra/db/data/KundeRepository.java
@@ -1,8 +1,12 @@
 package de.hhu.propra.db.data;
 
+import de.hhu.propra.db.entities.Auto;
 import de.hhu.propra.db.entities.Kunde;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface KundeRepository extends CrudRepository<Kunde, Long> {
     public List<Kunde> findAll();
+    public List<Auto> findAutoByFarbe();
 }
