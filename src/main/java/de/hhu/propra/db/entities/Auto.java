53ac7c452451a8407837608f7331e9a9beaaea4b
flfis102 <44257508+flfis102@users.noreply.github.com>
2018-11-15 14:50:08 +0100

    added Entity Auto

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -1,5 +1,16 @@
 package de.hhu.propra.db.entities;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
 public class Auto{
 
+    private Long id;
+
+    private String marke;
+    private String farbe;
+    private Integer hubraum;
 }
