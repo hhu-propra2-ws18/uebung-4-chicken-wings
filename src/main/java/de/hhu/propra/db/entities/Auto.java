95d1c230c6b878c47fd52ccfb500a00b04c2ceab
flfis102 <44257508+flfis102@users.noreply.github.com>
2018-11-15 15:05:59 +0100

    Populated Database

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -3,11 +3,14 @@ package de.hhu.propra.db.entities;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
 @Entity
 public class Auto{
-
+    @Id
+    @GeneratedValue
     private Long id;
 
     private String marke;
